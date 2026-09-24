7816bfd57d4fcf77f8b8ae41cccd618205a9c9b0
HeyItsArsh <ali.arsh11102005@gmail.com>
2026-09-24 11:40:33 +0530

    Change Made to Number of Students

--- main.tf
+++ main.tf
@@ -4,5 +4,5 @@ terraform {
 
 resource "local_file" "learning_system" {
   filename = "learning_system.txt"
-  content  = "Learning Management System\nCourses: 5\nStudents: 100"
+  content  = "Learning Management System\nCourses: 5\nStudents: 150"
 }
